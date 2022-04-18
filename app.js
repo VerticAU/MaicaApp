f8c104ab212a613713cfa6ff031cae441056098c
Dmitry Lisovsky <dmitry.lisovsky@vertic.com.au>
2022-04-18 23:51:04 +0300

    Bearer Token

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
+const bearerToken = require('express-bearer-token');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,8 +16,25 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const accessToken = process.env.ACCESS_TOKEN || 'provide the access token';
+
+app.use(bearerToken());
+app.use(function (req, res, next) {
+
+    console.log('token', req.token);
+    console.log('env token', accessToken);
+
+    if(accessToken !== req.token){
+        res.send('Access Denied!');
+        res.status(400).end();
+    } else {
+        next();
+    }
+
+});
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,19 +42,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
